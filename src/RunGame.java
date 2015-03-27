a273311bc1f51db36410755a19c1b785c02af8ed
Julia Edholm <dramagirl1012@yahoo.com>
2015-03-26 22:28:49 -0400

    updated RunGame

--- src/RunGame.java
+++ src/RunGame.java
@@ -5,6 +5,7 @@ import java.util.*;
  * 2. Create GP and have it initialize board
  * 3. Create GUI/build board
  * 4. Create front end interface
+ * 
  */
 public class RunGame {
 	private static Scanner sc = new Scanner(System.in);
@@ -31,9 +32,13 @@ public class RunGame {
 	private int[][] tradeResources; //tradeResources[0]= {type you want, amount, playerID}, tradeResouces[1] = {type you'll give away, amount, playerID}
 	private int[] yopResources;
 	
+	boolean AI;//how to mimic one player as AI
+	NoPeople np;
 	private boolean printRunningMessage = true;
 	
-	public RunGame(int numPlayers, boolean useGraphics){
+	public RunGame(int numPlayers, boolean useGraphics, boolean ai){
+		AI = ai;
+		System.out.println("AI "+AI);
 		players = new Player[numPlayers+1];
 		
 		for(int i=1; i<(numPlayers+1); i++)
@@ -62,11 +67,14 @@ public class RunGame {
 			//FEI will draw the graph
 			fei = new FrontEndInterface (this, board, numPlayers, ports, true);
 			fei.currentPlayerID = currentPlayerID;
+			if (AI){
+				np = new NoPeople(this, gl);
+			}
 		} else{
 			// run the game without graphics and with a random AI making all choices
 		//	fei = new FrontEndInterface (this, board, numPlayers, ports, false);
 			//fei.currentPlayerID = currentPlayerID;
-			NoPeople np = new NoPeople(this, gl);
+			np = new NoPeople(this, gl);
 			for (int i=0; i<playerCount*2; i++){
 				np.firstRoundPlaceSettlement();
 			}
@@ -77,7 +85,6 @@ public class RunGame {
 				int[] r = rollDice();
 				/*
 				if (printRunningMessage){
-				 
 					System.out.println("Next Round. Roll is: "+(r[0]+r[1]));
 				}
 				 */
@@ -101,7 +108,6 @@ public class RunGame {
 		currentPlayerID = order.getNextPlayer();
 		if (usingGraphics){
 			fei.updateCurrentPlayer(currentPlayerID);
-			clearVerticesAndAction();
 		}
 		int r1 = roll();
 		int r2 = roll();
@@ -110,6 +116,9 @@ public class RunGame {
 		}
 		gl.diceRoll(r1+r2);
 		updateAllStats();
+		if (AI){
+			np.turn(currentPlayerID);
+		}
 		return new int[] {r1,r2};
 	}
 	
@@ -237,7 +246,7 @@ public class RunGame {
 		//TEST:
 		//players[currentPlayerID].giveSettlementResources();
 		boolean success = 	gl.buildSettlement(currentPlayerID, v);
-		if (success){
+		if (success && usingGraphics){
 			fei.drawSettlement(v);
 		}
 		clearVerticesAndAction();
@@ -400,7 +409,7 @@ public class RunGame {
 	private boolean gameEnd(){
 		for (int i = 1; i< players.length; i++){
 			int vp = players[i].victoryPoints;
-			if (vp>= 3){
+			if (vp>= 4){
 				System.out.println("GAME OVER. Winner is Player " + i);
 				if (printRunningMessage){
 					players[i].printStats();
