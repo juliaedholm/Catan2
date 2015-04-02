533651141804fa0449bfd6309a510d320cf47b5b
julia-pearson <julia.pearson@gmail.com>
2015-04-02 13:51:42 -0400

    pop up

--- src/Clicks.java
+++ src/Clicks.java
@@ -157,8 +157,9 @@ public class Clicks implements MouseListener{
 		}
 		
 		//Julia P: we need a method that checks if they wanted to clear all clicks
-		boolean clickToClear = false;
+		boolean clickToClear = clickToClear(x,y);
 		if (clickToClear){
+			System.out.println("clear");
 			interaction.nullClick();
 		}
 	}
@@ -439,6 +440,15 @@ public class Clicks implements MouseListener{
 			return false;
 		}
 	}
+	public boolean clickToClear(int x, int y){
+		if(x<100 &&  y<100 ){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
 
 
 
