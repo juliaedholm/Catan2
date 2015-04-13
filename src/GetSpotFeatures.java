a9c18f5c7edccf940d48ff71b1378ca41608dac2
Julia Edholm <dramagirl1012@yahoo.com>
2015-04-13 15:00:27 -0400

    more features

--- src/GetSpotFeatures.java
+++ src/GetSpotFeatures.java
@@ -1,14 +1,30 @@
 /* class will take a vertex object and return the boolean prescence of  features in an array */
 
 public class GetSpotFeatures {
-	
-	public int[] getFeaturesFor (Vertex v){
+	/*
+	 * 0 = sheep
+	 */
+	public int[] getFeaturesForVertex (Vertex v){
+		boolean debug = false;
+		
 		int[] toReturn = new int[5];
+		if (sheep(v)){
+			toReturn[0] = 1;
+		}
+		
+		
+		if (debug){
+			System.out.println("features for vertex "+v.vertexNumber);
+			for (int i = 0; i<toReturn.length; i++){
+				System.out.println(toReturn[i]);
+			}
+		}
 		return toReturn;
 	}
 	
 	private boolean sheep(Vertex v){
 		//check graph if that vertex is on sheep. Return true if so
+	
 		return false;
 	}
 }
--- src/PlayManyGames.java
+++ src/PlayManyGames.java
@@ -5,6 +5,8 @@ public class PlayManyGames {
 		SpotQualityAlgorithm spotQuality = new SpotQualityAlgorithm ();
 		int numRuns = 10;
 		
+		GetSpotFeatures feats = new GetSpotFeatures();
+		
 		for (int j=0; j<numRuns; j++){
 			RunGame gameRunner = new RunGame(4, false, true, true); //will used a fixed board
 			int winningPlayer = gameRunner.runGameWithAI(false);
@@ -18,9 +20,9 @@ public class PlayManyGames {
 				int spot1 = playerStartSettlements[1];
 				int spot2 = playerStartSettlements[2];
 				if (i == winningPlayer){
-					System.out.println("Incrimenting");
 					Vertex v1 = verticesInGraph[spot1];
 					/*  incriment feature weights for the winning player */
+					int[] features = feats.getFeaturesForVertex (v1);
 					spotQuality.incrimentVertexNumWeight(spot1);
 					spotQuality.incrimentVertexNumWeight(spot2);
 				}  else {
