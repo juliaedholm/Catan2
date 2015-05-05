b2410a3a5910093f9130369e469cb514e64fb816
Julia Edholm <dramagirl1012@yahoo.com>
2015-05-05 19:32:26 -0400

    Added the decision tree
    every time you click a vertex to build a settlement, the terminal will
    show the output of the decision tree. TRUE = good spot. FALSE = bad spot

---
+++ src/DecisionTree.java
@@ -0,0 +1,130 @@
+/* algorithm that determines the qulaity of a spot based on the learned decision tree */
+
+
+public class DecisionTree {
+	GetSpotFeatures featFinder;
+	GraphController graph;
+	
+	public DecisionTree(GraphController g){
+		featFinder = new GetSpotFeatures();
+		graph = g;
+	} 
+	
+	public boolean isSpotGood (CatanVertex v){
+		boolean debug = true;
+		boolean toReturn = false;
+		int[] feats = featFinder.getFeaturesForVertex(v, graph);
+		
+		//decision tree:
+		if (feats[4] == 1){
+			if(feats[1] == 1){
+				if (debug){
+					System.out.println("TRUE. Spot had: yes 4 (sheep) and yes 1(wheat).");
+				}
+				return true;
+			} else if (feats[1] == 0){
+				if (feats[0] == 1){
+					if (debug){
+						System.out.println("TRUE. Spot had: yes 4 (sheep), no 1 (wheat), yes 0 (rock).");
+					}
+					return true;
+				} else if (feats[0] == 0){
+					if (debug){
+						System.out.println("FALSE. Spot had: yes 4 (sheep), no 1 (wheat), no 0 (rock).");
+					}
+					return false;
+				}
+			}
+		} else if (feats[4] == 0){
+			if(feats[25] == 1) {
+				if (debug){
+					System.out.println("FALSE. Spot had: no 4 (sheep),  yes 25(on 1 tile).");
+				}
+				return false;
+			} else if (feats[25]== 0){
+				if (feats[38] == 1){
+					if (feats[43] == 1){
+						if (feats[33] == 1){
+							if (feats[22] == 1){
+								if (debug){
+									System.out.println("TRUE. Spot had: no 4 (sheep),  no 25(on 1 tile), yes 38, yes 43, yes 33, yes 22");
+								}
+								return true;
+							} else if (feats[22] == 0){
+								if (feats[17] == 1){
+									if (feats [28] == 1){
+										if (debug){
+											System.out.println("FALSE. Spot had: no 4 (sheep),  no 25(on 1 tile), yes 38, yes 43, yes 33, no 22, yes 17, yes 28");
+										}
+										return false;
+									} else if (feats[28] == 0){
+										if (feats[18] == 1){
+											if (debug){
+												System.out.println("FALSE. Spot had: no 4 (sheep),  no 25(on 1 tile), yes 38, yes 43, yes 33, no 22, yes 17, no 28, yes 18");
+											}
+											return false;
+										} else if (feats[18] == 0){
+											if (feats[70] == 1){
+												if (feats[8] == 1){
+													if (debug){
+														System.out.println("TRUE. Spot had: no 4 (sheep),  no 25(on 1 tile), yes 38, yes 43, yes 33, no 22, yes 17, no 28, no 18, yes 70, yes 8");
+													}
+													return true;
+												} else if (feats [8] == 0){
+													if (debug){
+														System.out.println("FALSE. Spot had: no 4 (sheep),  no 25(on 1 tile), yes 38, yes 43, yes 33, no 22, yes 17, no 28, no 18, yes 70, no 8");
+													}
+													return false;
+												}
+											} else if (feats[70] == 0){
+												if (debug){
+													System.out.println("TRUE. Spot had: no 4 (sheep),  no 25(on 1 tile), yes 38, yes 43, yes 33, no 22, yes 17, no 28, no 18, no 70");
+												}
+												return true;
+											}
+										}
+									}
+								} else if (feats[17] == 0){
+									if (feats[28] == 1){
+										if (debug){
+											System.out.println("TRUE. Spot had: no 4 (sheep),  no 25(on 1 tile), yes 38, yes 43, yes 33, no 22, no 17, yes 28");
+										}
+										return true;
+									} else if (feats[28] == 0){
+										if (debug){
+											System.out.println("FALSE. Spot had: no 4 (sheep),  no 25(on 1 tile), yes 38, yes 43, yes 33, no 22, no 17, no 28");
+										}
+										return false;
+									}
+								}
+							}
+						} else if (feats [33] == 0){
+							if (debug){
+								System.out.println("FALSE. Spot had: no 4 (sheep),  no 25(on 1 tile), yes 38, yes 43, no 33");
+							}
+							return false;
+						}
+					} else if (feats[43] == 0){
+						if (debug){
+							System.out.println("TRUE. Spot had: no 4 (sheep),  no 25(on 1 tile), yes 38 (2 or 12 sheep), no 43 (3 or 11 sheep)");
+						}
+						return true;
+					}
+				} else if (feats[38] == 0){
+					if (debug){
+						System.out.println("TRUE. Spot had: no 4 (sheep),  no 25 (on 1 tile), no 38 (2 or 12 sheep)");
+					}
+					return true;
+				}
+			}
+		}
+		if (debug){
+			System.out.println("Spot did not fall within any decision tree cases. Returning false");
+		}
+		
+		return toReturn;
+		
+	
+	}
+	
+}
